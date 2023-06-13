567cb371161eab532e0c46f21f60a574fe59d6fd
wukai <wu.vkcyan@gmail.com>
2023-06-13 14:43:09 +0800

    add docs

---
+++ docs/assets/index-bf13f140.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o,a as s}from"./index-62dd3934.js";const t={},a={class:"w-[100%]"},_=s("div",{class:"base_card"},null,-1),n=s("div",{class:"base_card"},null,-1),d=[_,n];function r(i,l){return c(),o("div",a,d)}const h=e(t,[["render",r]]);export{h as default};
